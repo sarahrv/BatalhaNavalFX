7fee04bf217c58ef907f54928bda9c7ac8208cf7
sarahrv <105382868+sarahrv@users.noreply.github.com>
2022-12-08 20:32:58 -0300

    primeiro commit

---
+++ src/main/java/com/example/batalhanavalfx/Board.java
@@ -0,0 +1,39 @@
+package com.example.batalhanavalfx;
+import java.util.Arrays;
+
+    public class Board {
+        private char[][] tabuleiro = new char[10][10];
+        private int[][] posicaoDefesa;
+        private int[][] posicaoAtaque;
+
+        public void teste(){
+            for (char[] chars : tabuleiro) {
+                for (int j = 0; j < tabuleiro.length; j++) {
+                    System.out.print(chars[j]);
+                }
+                System.out.println();
+            }
+        }
+
+        public int[][] getPosicaoDefesa() {
+            return posicaoDefesa;
+        }
+
+
+        public void setPosicaoDefesa(int[][] posicaoDefesa) {
+            this.posicaoDefesa = posicaoDefesa;
+        }
+
+        public int[][] getPosicaoAtaque() {
+            return posicaoAtaque;
+        }
+
+        public void setPosicaoAtaque(int[][] posicaoAtaque) {
+            this.posicaoAtaque = posicaoAtaque;
+        }
+        public void printarTabuleiro(){
+
+        }
+    }
+
+
---
+++ src/main/java/com/example/batalhanavalfx/Player.java
@@ -0,0 +1,5 @@
+package com.example.batalhanavalfx;
+
+public class Player {
+    private String nome;
+}
