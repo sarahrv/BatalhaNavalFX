b5e759c46ce53f84c1710891f492863e30e48154
sarahrv <105382868+sarahrv@users.noreply.github.com>
2022-12-11 22:32:36 -0300

    classe main

---
+++ src/main/java/com/example/batalhanavalfx/MainGame.java
@@ -0,0 +1,15 @@
+package com.example.batalhanavalfx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainGame extends Application {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
