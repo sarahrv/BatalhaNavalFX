a9c300e91551c93a542893c149a65fa71e70cded
julianaboudoux <julianabclisboa@hotmail.com>
2022-12-26 19:00:00 -0300

    pega as coordenadas x,y de cada botão clicado

--- src/main/java/com/example/batalhanavalfx/controller/TabuleiroController.java
+++ src/main/java/com/example/batalhanavalfx/controller/TabuleiroController.java
@@ -1,5 +1,6 @@
 package com.example.batalhanavalfx.controller;
 
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
@@ -9,7 +10,13 @@ import javafx.scene.layout.GridPane;
 public class TabuleiroController {
     @FXML private GridPane board;
 
-    public void getButtonsXY(EventHandler event){
-        //por que nao funciona?
+    public void getButtonsXY(ActionEvent event){
+        //pego a coordenada do botão que eu cliquei
+        Button clickedButton = (Button) event.getSource();
+        int row = GridPane.getRowIndex(clickedButton);
+        int collumn = GridPane.getColumnIndex(clickedButton);
+
             }
-}
+        }
+
+
